0c5c657656954405d064440a91e2b45751b31faf
Tonyk17 <2425402991@live.gctu.edu.gh>
2026-05-31 21:29:21 +0000

    Add files via upload

---
+++ Scripting Project 1.py
@@ -0,0 +1,31 @@
+
+
+def  main():
+    # File Name:Scripting Project 1.py
+    # Description: Printing out my initials
+    # Assignment Number:  2
+    #
+    # Name: Anthony Kofi Quansah
+    # STUDENT ID:2425402991
+    # Email: 2425402991@live.gctu.edu.gh
+    #
+    # I,ANTHONY KOFI QUANSAH, pledge that this assignment was done by myself
+    #  and I have not provided this code to any other student.
+
+
+    print("...AKQ")
+    print("")
+
+    print("...AAAAAAAAAAAA........KKK....KKK............QQ..........")
+    print("...AAAA...AAAAA........KKK...KKK...........QQ..QQ........")
+    print("...AAAA....AAAA........KKK..KKK...........QQ....QQ.......")
+    print("...AAA......AAA........KKK.KKK...........QQ......QQ......")
+    print("...AAAAAAAAAAAA........KKKKK.............QQ.......QQ.....")
+    print("...AAAAAAAAAAAA........KKKKK.............QQ.......QQ.....")
+    print("...AAAAAAAAAAAA........KKK.KKK...........QQ.......QQ.....")
+    print("...AAAA....AAAA........KKK..KKK...........QQ..QQ.QQ......")
+    print("...AAAA....AAAA...**...KKK...KKK....**.....QQ..QQQ.....**")
+    print("...AAAA....AAAA...**...KKK....KKK...**.......QQ..QQ....**")
+
+
+main()
